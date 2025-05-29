3e1428116734ebd51782a28d98159cd5766c2a02
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-29 01:14:21 -0300

    fix: updated-css-imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './index.css'
-import Header from '../components/layout/Header'
-import Content from '../components/layout/Content'
-import Footer from '../components/layout/Footer'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Content from './components/Content'
 
 function App() {
   return (
