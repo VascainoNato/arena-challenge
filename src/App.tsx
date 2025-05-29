6a1fe51ba21e6e47de0eaf08b1268b50f72798c5
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-29 14:29:32 -0300

    fix: removed-unused-footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import './index.css'
 import './App.css'
 import Header from './components/Header'
-import Footer from './components/Footer'
 import Content from './components/Content'
 import Toggle from './components/Toggle'
 
 function App() {
   return (
     <>
-      <div className="flex flex-col min-h-screen h-screen bg-[#F8F8F8]">
+      <div className="flex flex-col flex-1 bg-[#F8F8F8]">
         <Header />
         <Toggle/>
         <div className="flex-1 flex flex-col">
           <Content />
-        </div>
-        <div className='hidden md:flex'>
-          <Footer />
         </div>
       </div>
     </>
