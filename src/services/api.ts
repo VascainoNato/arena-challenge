47f72076df8425c1d0abb75d1b9846a23498d5b7
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-29 03:44:43 -0300

    fix: adjusted-api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-// We define the base URL of the Product Hunt GraphQL API.
 const API_URL = "https://api.producthunt.com/v2/api/graphql";
 
-// We create and export a custom Axios instance named 'api', this instante includes the base URL and default headers.
+console.log("Token usado:", import.meta.env.VITE_PRODUCT_HUNT_TOKEN);
+
 export const api = axios.create({
   baseURL: API_URL,
   headers: {
-    // We inject our personal access token (read from environment variable) into the Authorization header.
     Authorization: `Bearer ${import.meta.env.VITE_PRODUCT_HUNT_TOKEN}`,
-    // We specify that we are sending data in JSON format.
     "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
