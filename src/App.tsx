f110806f6cfac01a2890ed4cf8d2bca51c89dda7
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-29 01:53:28 -0300

    feat: added-toggle-component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Content from './components/Content'
+import Toggle from './components/Toggle'
 
 function App() {
   return (
     <>
-      <div className="flex flex-col min-h-screen h-screen overflow-hidden">
+      <div className="flex flex-col min-h-screen h-screen overflow-hidden global-background-color">
         <Header />
+        <Toggle/>
         <div className="flex-1 flex flex-col overflow-hidden">
           <Content />
         </div>
