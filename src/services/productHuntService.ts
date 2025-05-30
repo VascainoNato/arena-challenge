0f77f2072384ce7e11c1fa74ebb36f1053d0590e
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-30 04:37:36 -0300

    feat: added-new-informations-on-the-main-post-query

--- src/services/productHuntService.ts
+++ src/services/productHuntService.ts
@@ -10,27 +10,39 @@ export const fetchPosts = async (
     // The first 20 defines the number of posts to retrieve.
     // If we have a cursor, we add it to fetch the next page of results.
   const query = `
-    query {
-      posts(order: ${order}, first: 25${cursor ? `, after: "${cursor}"` : ""}) {
-        edges {
-          node {
+  query {
+    posts(order: ${order}, first: 25${cursor ? `, after: "${cursor}"` : ""}) {
+      edges {
+        node {
+          id
+          name
+          tagline
+          description
+          votesCount
+          thumbnail {
+            url
+          }
+          media {
+            url
+            type
+          }
+          makers {
             id
             name
-            tagline
-            description
-            votesCount
-            thumbnail {
-              url
-            }
+            username
+            profileImage
           }
-          cursor
+          slug
+          website
         }
-        pageInfo {
-          hasNextPage
-        }
+        cursor
+      }
+      pageInfo {
+        hasNextPage
       }
     }
-  `;
+  }
+`;
   // Why do we use Edges and Nodes in Query? It's a GraphQL pagination format, known as RCS - Edges are the lists of elements, Node is the content inside each item and the Cursor is used for pagination/display.
   // We send a Post to the API with the query in the request body
   const res = await api.post("", { query });
